b4cbe2fe387d24f5a788e3876e83879af68d8393
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2024-01-02 12:02:04 +0530

    Positve OR Negative Done !!

---
+++ Variables&DataTypes/PositiveORNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PositiveORNegative 
+{
+    public static void main (String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int number=sc.nextInt();
+
+        if(number>=0)
+        {
+            System.out.println("Number is POSITIVE ");
+        }
+        else
+        {
+            System.out.println("Number is NEGATIVE ");
+        }
+
+    }
+    
+}
