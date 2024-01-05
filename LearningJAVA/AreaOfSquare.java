c39acffb1758de26d0ed22be5616a6b0a28320c8
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2024-01-05 11:48:21 +0530

    Directory Changed !!

---
+++ LearningJAVA/AreaOfSquare.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AreaOfSquare 
+{
+    public static void main(String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float side=sc.nextFloat();
+        float Area=side*side;
+        System.out.println(Area);
+    }
+    
+}
