caddb7bbccfb1cfb511b62676bd6070b1ea15f77
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2024-01-07 10:35:21 +0530

    Factorial Done

---
+++ LearningJAVA/FactorialOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FactorialOfNumber
+{
+    public static void main (String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        long num=sc.nextLong();    
+        long fact=1;
+        for(long i=1;i<=num;i++)
+        {
+            fact*=i;
+        }
+        System.out.println("Factorial is : "+fact);
+    }
+}
