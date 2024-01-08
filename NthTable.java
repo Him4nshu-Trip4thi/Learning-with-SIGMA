ed2e9ec74cfcc970f817c84f2efb8a38ce62e989
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2024-01-08 10:16:16 +0530

    Nth Table Done !!

---
+++ NthTable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class NthTable
+{
+    public static void main (String Args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter No. : ");
+        int num=sc.nextInt();
+
+        for(int i=1;i<=10;i++)
+        {
+            int product=num*i;
+            System.out.println(num + " " + "x" + " " + i + " " + "=" + " " +product);
+        }
+    }
+}
