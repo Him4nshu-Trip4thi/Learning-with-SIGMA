8cec230871e37efe0b677536e7f38b06e1a7da4f
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2024-01-10 02:39:34 +0530

    EvenORnot Function Done !!

---
+++ LearningJAVA/EvenORnotFunc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenORnotFunc 
+{
+    public static boolean EvenORnot(int n)
+    {
+        return (n%2==0);
+    }
+    public static void main (String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter No. : ");
+        int num=sc.nextInt();
+        System.out.println(EvenORnot(num));
+
+    }
+    
+}
