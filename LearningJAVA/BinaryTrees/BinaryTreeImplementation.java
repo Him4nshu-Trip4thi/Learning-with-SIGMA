c2fe28f54cc804908efd20ba85798cef7d1f6c48
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2025-07-23 12:06:09 +0530

    Level order traversal implemented

--- LearningJAVA/BinaryTrees/BinaryTreeImplementation.java
+++ LearningJAVA/BinaryTrees/BinaryTreeImplementation.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeImplementation{
     static class Node{
         int data;
@@ -48,7 +51,31 @@ public class BinaryTreeImplementation{
 
         }
 
+        public static void levelOrder(Node root){
+            Queue<Node> q= new LinkedList<>();
 
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode= q.remove();
+
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }
+                    else{
+                        q.add(null);
+                    }
+                }
+                else{
+                    System.out.print(currNode.data + " ");
+                    if(currNode.left != null) q.add(currNode.left);
+                    if(currNode.right != null) q.add(currNode.right); 
+                }
+            }
+        }
     }
     public static void main (String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -61,6 +88,8 @@ public class BinaryTreeImplementation{
         System.out.print("The inOrder traversal of the nodes: "); tree.inOrder(root);
         System.out.println();
         System.out.print("The postOrder traversal of the nodes: "); tree.postOrder(root);
+        System.out.println();
+        System.out.println("The levelOrder traversal of the nodes: "); tree.levelOrder(root);
         
     }
 }
